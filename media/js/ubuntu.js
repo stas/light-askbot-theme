7311c35cda470c8ed64e917f4a7c2b6ac02e3c1f
Stas Sușcov <stas@nerd.ro>
2011-02-28 01:12:59 +0200

    Updated edit screen, question screen now works with some basic layout

--- media/js/ubuntu.js
+++ media/js/ubuntu.js
@@ -9,8 +9,11 @@ jQuery('#sidebar .inner').corner('round 5px keep;');
 jQuery('#content .tabBar').corner('round 5px keep;');
 jQuery('#content .evenMore').corner('round 5px keep;');
 jQuery('#content .user-content').corner('round 5px keep;');
-jQuery('#content .gravatar').corner('round 5px keep;');
+jQuery('.gravatar').corner('round 5px keep;');
+jQuery('#content .search-tips').corner('round 5px keep;');
 jQuery('#askform').corner('round 5px keep;');
+jQuery('#fmedit').corner('round 5px keep;');
+jQuery('#question-entry pre').corner('round 5px keep;');
 
 /* Add some awesome buttons */
 jQuery('#ab-meta-nav .user-micro-info').addClass('awesome');
@@ -19,4 +22,6 @@ jQuery('#content .avatar-link a').addClass('awesome');
 jQuery('#content .user-profile-tool-links a').addClass('awesome');
 jQuery('#content .user-content .medal').addClass('awesome');
 jQuery('#askform .preview-toggle span').addClass('awesome');
+jQuery('#fmedit .preview-toggle span').addClass('awesome');
+jQuery('#sidebar .info-box-follow-up-links a').addClass('awesome');
 
