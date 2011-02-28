d6d21efe220f76c213be971ae819dd650dcb8f27
Stas Sușcov <stas@nerd.ro>
2011-02-28 15:46:33 +0200

    Question screen, comments ready

--- media/js/ubuntu.js
+++ media/js/ubuntu.js
@@ -13,7 +13,10 @@ jQuery('.gravatar').corner('round 5px keep;');
 jQuery('#content .search-tips').corner('round 5px keep;');
 jQuery('#askform').corner('round 5px keep;');
 jQuery('#fmedit').corner('round 5px keep;');
-jQuery('#question-entry pre').corner('round 5px keep;');
+jQuery('#question-controls ul').corner('round 5px keep;');
+jQuery('.question-entry pre').corner('round 5px keep;');
+jQuery('.question-entry .post-update-info-container .author').corner('round 5px keep;');
+
 
 /* Add some awesome buttons */
 jQuery('#ab-meta-nav .user-micro-info').addClass('awesome');
@@ -24,4 +27,6 @@ jQuery('#content .user-content .medal').addClass('awesome');
 jQuery('#askform .preview-toggle span').addClass('awesome');
 jQuery('#fmedit .preview-toggle span').addClass('awesome');
 jQuery('#sidebar .info-box-follow-up-links a').addClass('awesome');
+jQuery('.comments a.button').addClass('awesome');
+jQuery('.content .comment .edit').addClass('awesome');
 
