1de3c1b7af33f8644d227295726688d64e750d44
Stas Sușcov <stas@nerd.ro>
2011-03-23 13:51:46 +0200

    Most of the ajax stuff works again

--- media/js/ubuntu.js
+++ media/js/ubuntu.js
@@ -10,10 +10,9 @@ jQuery('#content.wide').corner('round 5px keep;');
 jQuery('#content .inner.auth').corner('round 5px keep;');
 jQuery('#content .tabBar').corner('round 5px keep;');
 jQuery('#content .evenMore').corner('round 5px keep;');
-jQuery('#content .user-content').corner('round 5px keep;');
 jQuery('.gravatar').corner('round 5px keep;');
 jQuery('#content .search-tips').corner('round 5px keep;');
-jQuery('#askform').corner('round 5px keep;');
+jQuery('#content > #askform').corner('round 5px keep;');
 jQuery('#fmedit').corner('round 5px keep;');
 jQuery('#question-controls ul').corner('round 5px keep;');
 jQuery('.question-entry pre').corner('round 5px keep;');
@@ -23,7 +22,6 @@ jQuery('.answer-controls ul').corner('round 5px keep;');
 jQuery('#question-list .short-summary').corner('round 5px keep;');
 jQuery('#content .inner.badges-page').corner('round 5px keep;');
 jQuery('#question-list .short-summary .item-count').corner('round 25px keep;');
-
 
 /* Add some awesome buttons */
 jQuery('#ab-meta-nav .user-micro-info').addClass('awesome');
